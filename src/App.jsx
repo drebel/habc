380d329f733c4f65cc02e577575a5d0770aa845e
drew <drewbeland@gmail.com>
2023-11-29 17:46:44 -0800

    title and instructions components

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Title from './components/Title.jsx'
+import Instructions from './components/Instructions.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Title />
+      <Instructions /> 
+
+      {/* gonna have a data.js file that we can have an array of objects where the properties are the data we want to pass in as props to our components
+      we import the array at the top of the file
+      then map over it and return custom component elements that will be our questions and inputs
+      i think maybe i will break it up into each subsection? 
+      */}
     </>
   )
 }
-
-export default App
