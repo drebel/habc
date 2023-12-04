85960a9503d1559db5ac8495284ce29967b60136
drew <drewbeland@gmail.com>
2023-12-04 11:27:07 -0800

    added title, instructions, form with one question, and js file of questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Title from './components/Title.jsx'
 import Instructions from './components/Instructions.jsx'
+import Form from './components/Form.jsx'
 
 export default function App() {
   return (
     <>
       <Title />
       <Instructions /> 
-
-      {/* gonna have a data.js file that we can have an array of objects where the properties are the data we want to pass in as props to our components
-      we import the array at the top of the file
-      then map over it and return custom component elements that will be our questions and inputs
-      i think maybe i will break it up into each subsection? 
-      */}
+      <Form />
     </>
   )
 }
