a7438ef21f7c5793b735b6b8329df46f2e00fa94
drew <drewbeland@gmail.com>
2023-12-09 22:01:27 -0800

    set up routes for auth/user controller

---
+++ server/routes/routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+// all the routes to controllers that talk to the api
+// you dont need to do routes that are only serving a page since its handled by react router
+
+// auth route for sign up log in log out
+// post route to auth to sign up
+router.post('/signup', authController.postSignup)
+// post route to auth to log in
+router.post('/login', authController.postlogin)
+// post route to auth to sign out
+router.post('/logout', authController.postLogout)
+
+
+// formdata to submit a new form
+// get route to to form controller to get all form data by a specific user
+// post route to form controller to submit a new response 
+
+export default router
